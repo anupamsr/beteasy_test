c18adf8c9576a1e3a0cef92f71a413c2f0fca4df
Anupam Srivastava <anupam@dont.tech>
2019-12-29 14:16:14 +0530

    Basic structure

--- dotnet-code-challenge/Program.cs
+++ dotnet-code-challenge/Program.cs
@@ -4,6 +4,53 @@ namespace dotnet_code_challenge
 {
     class Program
     {
+        public interface IDataProcessor
+        {
+            bool Load(in string resource);
+        }
+
+        public class XMLProcessor : IDataProcessor
+        {
+            public XMLProcessor() { }
+
+            public bool Load(in string resource)
+            {
+                return false;
+            }
+        }
+
+        public class JSONProcessor : IDataProcessor
+        {
+            public JSONProcessor() { }
+
+            public bool Load(in string resource)
+            {
+                return false;
+            }
+        }
+
+        class Feed
+        {
+            Feed(in string resource)
+            {
+                xmlproc = new XMLProcessor();
+                jsonproc = new JSONProcessor();
+                bool isParsed = xmlproc.Load(resource);
+                if (!isParsed)
+                {
+                    jsonproc.Load(resource);
+                    proc = jsonproc;
+                }
+                else
+                {
+                    proc = xmlproc;
+                }
+            }
+            private static XMLProcessor xmlproc;
+            private static JSONProcessor jsonproc;
+            private static IDataProcessor proc;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
