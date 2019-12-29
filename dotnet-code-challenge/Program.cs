cd48e210027b36c447547cfe6470c1e0594fd35a
Anupam Srivastava <anupam@dont.tech>
2019-12-29 15:08:11 +0530

    Support for URI

--- dotnet-code-challenge/Program.cs
+++ dotnet-code-challenge/Program.cs
@@ -1,25 +1,60 @@
 ﻿using System;
 using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace dotnet_code_challenge
 {
     partial class Program
     {
+        public class UriProcessor
+        {
+            public async Task<string> Fetch(string uri)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    return await httpClient.GetStringAsync(uri);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             if (args.Length < 1)
             {
                 Console.Error.WriteLine("At least one argument to a resource must be specified");
             }
-            foreach (string path in args)
+            foreach (string arg in args)
             {
-                Console.WriteLine("Processing: " + path);
-                string fileContent = File.ReadAllText(path);
-                Feed feed = new Feed(fileContent);
-                var price_sorted_horses = feed.GetPriceSortedHorseList();
-                foreach (Horse horse in price_sorted_horses)
+                try
                 {
-                    Console.WriteLine(horse.Name); // Print only horse name
+                    Console.WriteLine("Processing: " + arg);
+                    UriProcessor client = new UriProcessor();
+                    string fileContent = null;
+                    try
+                    {
+                        Task<string> task = client.Fetch(arg);
+                        task.Wait();
+                        fileContent = task.Result;
+                    }
+                    catch (AggregateException)
+                    {
+                        // Try to read it as a file?
+                        // Required as http(s) is inaccissible outside of Australia/NZ
+                        // So I couldn't test the http part
+                        fileContent = File.ReadAllText(arg);
+                    }
+
+                    Feed resource = new Feed(fileContent);
+                    var price_sorted_horses = resource.GetPriceSortedHorseList();
+                    foreach (Horse horse in price_sorted_horses)
+                    {
+                        Console.WriteLine(horse.Name); // Print only horse name
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e.Message);
                 }
             }
 
